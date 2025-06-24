fd7a95e968a1a9064c9523ccb69295a7ea0a0a6d
Yamkela Matolengwe <matolematics07@gmail.com>
2025-06-24 11:13:45 +0200

    Setup2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements(file_path:str):
+def get_requirements(file_path:str)-> List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
